61f3ca5685dcc6f11f06f63f0ef8b32e14c4fa63
vaisakhg0 <vaisakhg30@gmail.com>
2022-05-17 12:17:38 +0530

    books

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Addbooks from './component/Addbooks';
+
+function App() {
+  return (
+    <div>
+   
+<Addbooks/>
+
+    </div>
+  );
+}
+
+export default App;
